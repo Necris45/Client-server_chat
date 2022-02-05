19425eff346abf587955bed656d09dfb6456b2a0
Necris45 <necris01@mail.ru>
2022-02-05 17:55:45 +0500

    Done 7 dz

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 """Программа-сервер"""
 
 import sys
-import json
+import time
+import argparse
+import select
 from logging import getLogger
 from socket import socket, AF_INET, SOCK_STREAM
-from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, PRESENCE, ERROR, DEFAULT_PORT, MESSAGE, \
+    MESSAGE_TEXT, SENDER, TIME, USER
 from common.utils import get_message, send_message
 from logs import server_log_config
 from decorator import log
@@ -14,22 +16,43 @@ logs = getLogger('server')
 
 
 @log
-def process_client_message(message):
+def process_client_message(message, messages_list, client):
     """
     Обработчик сообщений от клиентов, принимает словарь-сообщение от клинта, проверяет корректность,
     возвращает словарь-ответ для клиента
     :param message:
+    :param messages_list:
+    :param client:
     :return:
     """
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message and \
+            message[USER][ACCOUNT_NAME] == 'Guest':
         logs.info(f'Клиент {USER} подключился к серверу')
-        return {RESPONSE: 200}
-    logs.error(f'Запрос содержит ошибку: {message}')
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+        send_message(client, {RESPONSE: 200})
+    elif ACTION in message and message[ACTION] == MESSAGE and TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+    else:
+        send_message(client, {RESPONSE: 400, ERROR: 'Bad Request'})
+        logs.error('Клиент отправил некорректный запрос на подключение')
+
+
+@log
+def arg_parser():
+    """Парсер аргументов коммандной строки"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    # проверка получения корретного номера порта для работы сервера.
+    if not 1023 < listen_port < 65536:
+        logs.critical(f'Попытка запуска сервера с указанием неподходящего порта {listen_port}. '
+                      f'Допустимы адреса с 1024 до 65535.')
+        sys.exit(1)
+
+    return listen_address, listen_port
 
 
 def main():
@@ -39,48 +62,52 @@ def main():
     server.py -p 8079 -a 127.0.0.1
     :return:
     """
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        logs.critical('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        logs.critical('В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-    # Затем загружаем какой адрес слушать
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-    except IndexError:
-        logs.critical('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
-        sys.exit(1)
+    listen_address, listen_port = arg_parser()
     logs.info(f'Запущен сервер, порт для подключений: {listen_port}, '
-              f'адрес с которого принимаются подключения: {listen_address}. '
-              f'Если адрес не указан, принимаются соединения с любых адресов.')
-    # Готовим сокет
+              f'адрес с которого принимаются подключения: {listen_address}. ')
+    # Готовим сокет, выставляем таймаут и создаем списки клиентов и сообщений
     transport = socket(AF_INET, SOCK_STREAM)
     transport.bind((listen_address, listen_port))
+    transport.settimeout(0.5)
+    clients_lst = []
+    messages_lst = []
     # Слушаем порт
     transport.listen(MAX_CONNECTIONS)
     while True:
-        client, client_address = transport.accept()
         try:
-            message_from_client = get_message(client)
-            logs.debug(message_from_client)
-            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
-            response = process_client_message(message_from_client)
-            send_message(client, response)
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            logs.error('Принято некорретное сообщение от клиента.')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError:
+            pass
+        else:
+            logs.info(f'Установлено соединение с {client_address}')
+            clients_lst.append(client)
+        # создаем переменные для принятия данных от select
+        recv_lst = []
+        send_lst = []
+        err_lst = []
+        try:
+            if clients_lst:
+                # recv_lst-список клиентов, отправивших сообщение,send_lst- список клиентов,ждущих сообщение
+                recv_lst, send_lst, err_lst = select.select(clients_lst, clients_lst, [], 0)
+        except OSError:
+            pass
+        if recv_lst:
+            for client_with_messages in recv_lst:
+                try:
+                    process_client_message(get_message(client_with_messages), messages_lst, client_with_messages)
+                except:
+                    logs.info(f'Клиент {client_with_messages.getpeername()} отключился от сервера.')
+                    clients_lst.remove(client_with_messages)
+        if messages_lst and send_lst:
+            message = {ACTION: MESSAGE, SENDER: messages_lst[0][0],
+                       TIME: time.time(), MESSAGE_TEXT: messages_lst[0][1]}
+            del messages_lst[0]
+            for expect_client in send_lst:
+                try:
+                    send_message(expect_client, message)
+                except:
+                    logs.info(f'Клиент {expect_client.getpeername()} отключился')
+                    clients_lst.remove(expect_client)
 
 
 if __name__ == '__main__':
